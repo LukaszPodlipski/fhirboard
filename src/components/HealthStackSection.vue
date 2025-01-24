4005250712cd760f88f284ee7674b45038f6b1d1
Łukasz Podlipski <lukaszpodlipskikontakt@gmail.com>
2025-01-24 12:35:52 +0100

    Improve styling

--- src/components/HealthStackSection.vue
+++ src/components/HealthStackSection.vue
@@ -4,24 +4,24 @@
       <h2 class="text-header-2xl leading-10 text-center">
         How does it speed up your work with FHIR?
       </h2>
-      <div class="flex flex-col justify-center gap-2 max-w-[650px] center">
+      <div class="flex flex-col justify-center gap-2 max-w-[650px] mt-12">
         <p class="text-header-m text-center">FHIR Board accelerates work by</p>
         <ul>
           <li class="text-caption-s">
-            - Automatically converting FHIR ViewDefinitions to SQL queries.
+            • Automatically converting FHIR ViewDefinitions to SQL queries.
           </li>
           <li class="text-caption-s">
-            - Providing instant data visualization through Apache Superset integration.
+            • Providing instant data visualization through Apache Superset integration.
           </li>
           <li class="text-caption-s">
-            - Enabling quick combination of different FHIR resources through saved views.
+            • Enabling quick combination of different FHIR resources through saved views.
           </li>
           <li class="text-caption-s">
-            - Including predefined use cases for immediate implementation
+            • Including predefined use cases for immediate implementation
           </li>
         </ul>
       </div>
-      <p class="text-header-m--bold-strong text-center mt-2">
+      <p class="text-header-m--bold-strong text-center mt-12">
         Additionally, our AI features allow generating ViewDefinitions from simple text
         descriptions, making FHIR data analysis even more accessible.
       </p>
@@ -32,7 +32,7 @@
 <style scoped lang="scss">
 .hero-section {
   width: 100%;
-  padding: 140px 36px;
+  padding: 120px 36px;
   background: linear-gradient(180deg, rgba(255, 255, 255, 1) 0%, rgba(98, 169, 240, 0.298) 100%);
   display: flex;
   flex-direction: column;
