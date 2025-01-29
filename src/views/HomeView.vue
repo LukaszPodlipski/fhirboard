f9cd177742d648b365db41019cfec7390459bf2a
Łukasz Podlipski <lukaszpodlipskikontakt@gmail.com>
2025-01-29 16:08:55 +0100

    Change sections components names, git url and Momentum description

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,19 @@
 <script setup>
-import HeroSection from '@/components/HeroSection.vue'
-import HealthStackSection from '@/components/HealthStackSection.vue'
-import HowItWorksSection from '@/components/HowItWorksSection.vue'
-import Experts from '@/components/Experts.vue'
-import Infrastructure from '@/components/Infrastructure.vue'
-import Contact from '@/components/Contact.vue'
+import HeroSection from '@/components/sections/HeroSection.vue'
+import FeaturesOverviewSection from '@/components/sections/FeaturesOverviewSection.vue'
+import HowItWorksSection from '@/components/sections/HowItWorksSection.vue'
+import ExpertsSection from '@/components/sections/ExpertsSection.vue'
+import UseCasesSection from '@/components/sections/UseCasesSection.vue'
+import ContactSection from '@/components/sections/ContactSection.vue'
 </script>
 
 <template>
   <div class="mt-20">
     <HeroSection />
-    <HealthStackSection />
+    <FeaturesOverviewSection />
     <HowItWorksSection />
-    <Experts />
-    <Infrastructure />
-    <Contact />
+    <ExpertsSection />
+    <UseCasesSection />
+    <ContactSection />
   </div>
 </template>
